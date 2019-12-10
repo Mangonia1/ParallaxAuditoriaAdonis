e530dccb7abf5aa7763a57f1c61edd1216fd5fd4
RAUL EDUARDO PEREZ SALDIVAR <rauleduardo16@hotmail.com>
2019-12-09 23:30:50 -0600

    conf de env

--- config/database.js
+++ config/database.js
@@ -75,9 +75,9 @@ module.exports = {
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', 'postgres'),
+      password: Env.get('DB_PASSWORD', '1234'),
+      database: Env.get('DB_DATABASE', 'PGAUDIT')
     },
     debug: Env.get('DB_DEBUG', false)
   }
